a0c333b84b0227c557c4aa90582e1493fe104124
Matthias <mail@matthiasbock.net>
2013-06-23 22:04:53 +0200

    tried LiquidCrystal.h

--- M1632/M1632.ino
+++ M1632/M1632.ino
@@ -1,35 +1,14 @@
-int[6] DB = {0,1,2,3,4,5,6,7};
+int DB[8] = {0,1,2,3,4,5,6,7};
 int RS = 8;
 int RW = 9;
 int E = 10;
 
-// mode
-int INSTRUCTION = 0;
-int DATA = 1;
-
-void setup() {
-  pinMode(RS, OUTPUT);
-  pinMode(RW, OUTPUT);
-  pinMode(E, OUTPUT);
-  pinMode(DB[0], OUTPUT);
-  pinMode(DB[1], OUTPUT);
-  pinMode(DB[2], OUTPUT);
-  pinMode(DB[3], OUTPUT);
-  pinMode(DB[4], OUTPUT);
-  pinMode(DB[5], OUTPUT);
-  pinMode(DB[6], OUTPUT);
-  pinMode(DB[7], OUTPUT);
-}
-
-void mode(int m) {
-  setPin(RS, m);
-}
-
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(2, 3, 4, 9, 10, 11, 12);
+LiquidCrystal lcd(RS, RW, E, DB[4], DB[5], DB[6], DB[7]);
 
 void setup(){
   lcd.print("hello, world!");
 }
-void loop() {}
+void loop() {
+}
