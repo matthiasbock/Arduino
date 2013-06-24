fd28f16b8c400cbd05fc1433b45425a189dec1c3
Matthias <mail@matthiasbock.net>
2013-06-24 21:22:08 +0200

    still doesn't work

--- M1632/M1632.ino
+++ M1632/M1632.ino
@@ -1,15 +1,31 @@
-int DB[8] = {45,43,41,39,37,35};
-int RS = 33;
-int RW = 31;
-int E = 29;
+int d[8] = {37,36,35,34,33,32,31,30};
+int enable = 40;
+int rw = 41;
+int rs = 42;
+
+//
+// http://arduino.cc/de/Reference/LiquidCrystalConstructor
+//
 
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(RS, RW, E, DB[4], DB[5], DB[6], DB[7]);
+LiquidCrystal lcd(rs, rw, enable, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
 
 void setup(){
+  pinMode(rs, OUTPUT);
+  pinMode(rw, OUTPUT);
+  pinMode(enable, OUTPUT);
+  pinMode(d[0], OUTPUT);
+  pinMode(d[1], OUTPUT);
+  pinMode(d[2], OUTPUT);
+  pinMode(d[3], OUTPUT);
+  pinMode(d[4], OUTPUT);
+  pinMode(d[5], OUTPUT);
+  pinMode(d[6], OUTPUT);
+  pinMode(d[7], OUTPUT);
+  delay(1000);
   lcd.print("hello, world!");
 }
+
 void loop() {
-  lcd.print("hello, world!");
 }
